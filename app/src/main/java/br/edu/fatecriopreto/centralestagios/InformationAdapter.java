9c37ff510e6bffb28dec34494c58baabf2700779
hcbarbosa <hannacarolinabarbosa@gmail.com>
2015-04-23 10:34:31 -0300

    Interface main, menu

--- app/src/main/java/br/edu/fatecriopreto/centralestagios/InformationAdapter.java
+++ app/src/main/java/br/edu/fatecriopreto/centralestagios/InformationAdapter.java
@@ -1,32 +1,52 @@
 package br.edu.fatecriopreto.centralestagios;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
 
 
 public class InformationAdapter extends RecyclerView.Adapter<InformationAdapter.MyViewHolder>{
 
+    private final LayoutInflater inflater;
+    List<Information> listMenu = Collections.emptyList();
+    public InformationAdapter(Context context, List<Information> lista){
+       inflater = LayoutInflater.from(context);
+       this.listMenu = lista;
+    }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.custom_row, parent, false);
+        //MyViewHolder holder = new MyViewHolder(view);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        Information atual = listMenu.get(position);
+        holder.title.setText(atual.title);
+        holder.icon.setImageResource(atual.iconid);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listMenu.size();
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder{
-
+        TextView title;
+        ImageView icon;
         public MyViewHolder(View itemView) {
             super(itemView);
+            title = (TextView) itemView.findViewById(R.id.listText);
+            icon = (ImageView) itemView.findViewById(R.id.listIcon);
         }
     }
 }
--- app/src/main/java/br/edu/fatecriopreto/centralestagios/NavigationDrawerFragment.java
+++ app/src/main/java/br/edu/fatecriopreto/centralestagios/NavigationDrawerFragment.java
@@ -7,12 +7,16 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -25,6 +29,7 @@ public class NavigationDrawerFragment extends Fragment {
     public static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
+    private InformationAdapter adapter;
 
     private boolean mUserLearnedDrawer;
     private boolean mFromSavedInstanceState;
@@ -49,9 +54,29 @@ public class NavigationDrawerFragment extends Fragment {
         // Inflate the layout for this fragment
         View layout = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
         recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
-
-
+        adapter = new InformationAdapter(getActivity(),getListaMenu());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return layout;
+    }
+
+    public static List<Information> getListaMenu(){
+        List<Information> listMenu = new ArrayList<>();
+        int[] icons = {R.drawable.ic_plusone_medium_off_client,R.drawable.ic_plusone_medium_off_client,
+                R.drawable.ic_plusone_medium_off_client,R.drawable.ic_plusone_medium_off_client,
+                R.drawable.ic_plusone_medium_off_client,R.drawable.ic_plusone_medium_off_client,
+                R.drawable.ic_plusone_medium_off_client};
+        String[] titles = {"Inicio","Perfil","Curriculo","Vagas", "Mensagens","Configuracoes","Sair"};
+
+        for(int i = 0; i < icons.length && i < titles.length; i++)
+        {
+            Information info = new Information();
+            info.iconid = icons[i];
+            info.title = titles[i];
+            listMenu.add(info);
+        }
+
+        return listMenu;
     }
 
 
