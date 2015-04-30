b96d155142a1e0437b7f66efc80c05351b6313ca
Arian <arianbotine@hotmail.com>
2015-04-29 23:20:16 -0300

    Eu fiz o esquema do DBHelper la ;) s2

--- app/src/main/java/br/edu/fatecriopreto/centralestagios/Perfil.java
+++ app/src/main/java/br/edu/fatecriopreto/centralestagios/Perfil.java
@@ -4,4 +4,124 @@ package br.edu.fatecriopreto.centralestagios;
  * Created by Arian on 28/04/2015.
  */
 public class Perfil {
+
+    private long rm;
+    private long cursoId;
+    private String cidade;
+    private String telefone;
+    private long CEP;
+    private long ano;
+    private String uf;
+    private String bairro;
+    private String logradouro;
+    private String complemento;
+    private String nome;
+    private String email;
+    private String semestre;
+
+    public long getRm() {
+        return rm;
+    }
+
+    public void setRm(long rm) {
+        this.rm = rm;
+    }
+
+    public long getCursoId() {
+        return cursoId;
+    }
+
+    public void setCursoId(long cursoId) {
+        this.cursoId = cursoId;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public long getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(long CEP) {
+        this.CEP = CEP;
+    }
+
+    public long getAno() {
+        return ano;
+    }
+
+    public void setAno(long ano) {
+        this.ano = ano;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+
 }
+
